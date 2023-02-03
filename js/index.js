dc6c5adf5b0febde1a7ee1e67467fb28e294c799
IvanUni <49763171+IvanUni@users.noreply.github.com>
2023-02-03 11:37:01 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let container = document.querySelector(`.transactions-container`);
+let result = document.querySelector(`#result`);
+
+// Данные
+let usernames = [`Вика`, `Саша`, `Никита`, `Настя`];
+let scores = [900, 888, 902, 898];
